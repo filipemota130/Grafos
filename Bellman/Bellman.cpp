cfc56f1023393b37bbcf5f80b9344bffe2a52c12
filipemota130 <fsmota@ic.ufal.br>
2022-12-11 16:46:07 -0300

    Release: Bellman Algorithm

--- Bellman/Bellman.cpp
+++ Bellman/Bellman.cpp
@@ -2,40 +2,38 @@
 #include <vector>
 #include <limits.h>
 #include <queue>
+
 using namespace std;
 
-int main()
-{
-    int n, m, src; 
-    cin >> n >> m >> src;
-    src--;
-
+int main(){
+    int n,m;cin >> n >> m;
     vector<vector<int>> edges;
-    for (int i = 0; i < m; i++) //Recebe 
-    {
-        int u, v, w;
+
+    for (int i=0;i<m;i++){
+        int u,v,w;
         cin >> u >> v >> w;
+        u--;v--;
         edges.push_back({u,v,w});
+        edges.push_back({v,u,w});
     }
-
-    //int src;
-    //cin >> src;
-    vector<int> dist(n, INT_MAX);
-    dist.at(src) = 0;
-
-    for (int i = 0; i < n-1; i++)
-    {
-        for (auto e: edges)
-        {
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
-            dist[v] = min(dist[v], w + dist[u]);
+    vector<int> dist(n,INT_MAX);
+    int src;
+    cin >> src;
+    dist[src-1] = 0;
+    for (int i=0;i<n;i++){
+        for (auto e: edges){
+            int u= e[0];
+            int v= e[1];
+            int w= e[2];
+            if (dist[u] !=INT_MAX && ((dist[u]+w) < dist[v])){
+                dist[v]= dist[u]+w;
+            }
         }
     }
-
-    for (auto i: dist)
-    {
-        cout << i << endl;
+    int count = 1;
+    for (auto i:dist){
+        cout << count << ":"<< i << " ";
+        count++;
     }
+    cout<<'\n';
 }
