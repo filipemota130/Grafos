47a58357b29ca0c81c2eca31a3027c9afec54903
filipemota130 <fsmota@ic.ufal.br>
2022-12-13 00:41:44 -0300

    Release: Backup of Prim

--- Prim_Second/prim_teste.cpp
+++ Prim_Second/prim_teste.cpp
@@ -2,14 +2,13 @@
 #include <limits.h>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
 typedef pair<int, int> iPair;
 
 void primMST(vector<vector<pair<int,int>>> Grafo, int V)
 {
-    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+    priority_queue <iPair, vector <iPair>, greater<iPair>> pq;
  
     int src = 0;
 
@@ -25,16 +24,16 @@ void primMST(vector<vector<pair<int,int>>> Grafo, int V)
     {
         int u = pq.top().second;
         pq.pop();
-        if(inMST[u] == true){
+        if(inMST[u] == true)
+        {
             continue;
         }
        
         inMST[u] = true;
-        vector< pair<int, int> >::iterator i;
-        for (i = Grafo[u].begin(); i != Grafo[u].end(); ++i)
+        for (auto edge:Grafo[u])
         {
-            int v = (*i).first;
-            int weight = (*i).second;
+            int v = edge.first;
+            int weight = edge.second;
             if (inMST[v] == false && key[v] > weight)
             {
                 key[v] = weight;
@@ -44,24 +43,27 @@ void primMST(vector<vector<pair<int,int>>> Grafo, int V)
         }
     }
     int sum = 0;
-    for (int i = 1; i < V; ++i){
-        cout<< "(" << parent[i]+1 << "," << i+1 <<")" << " ";
-        sum+=key[i];
+    for (int i = 1; i < V; ++i)
+    {
+        cout << "(" << parent[i] + 1 << "," << i+1 << ")" << " ";
+        sum += key[i];
     }
-    cout<< endl << "Peso Total da Arvore Geradora: " << sum << endl;
+    cout << endl << "Peso total da árvore geradora: " << sum << endl;
 }
 
+//PARA TESTES: Dinamizar o src
 int main()
 {
-    int n,m,v,u,w;
+    int n, m, v, u, w;
     cin >> n >> m;
-    vector<vector<pair<int,int>>> Grafo(n);
+    vector <vector<pair<int,int>>> Grafo(n);
     //Aceita Apenas Vértices possitivos >0
-    for(int i=0; i<m; i++){
+    for(int i = 0; i < m; i++)
+    {
         cin >> v >> u >> w;
-        v--; u--;
-        Grafo[v].push_back({u,w});
-        Grafo[u].push_back({v,w});
+        v--, u--;
+        Grafo[v].push_back({u, w});
+        Grafo[u].push_back({v, w});
     }
     primMST(Grafo, n);
     return 0;
